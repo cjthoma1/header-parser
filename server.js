570180830319b52d017f8f35838b9bea270a2397
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-05-31 20:01:01 +0000

    Rendering software properly

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var express = require("express"),
 app = express(),
-port = process.env.PORT || '8080';
+port = process.env.PORT || '8080',
+useragent = require('express-useragent'),
+locale = require("locale");
+
+
 app.enable('trust proxy');
+app.use(useragent.express());
 app.use((req, res) => {
+// res.header("Content-Type", "text/plain")
+
+var source = req.useragent.source,
+index1 = source.indexOf("("),
+index2 = source.indexOf(")")
+source = source.slice(index1+1, index2)
+
 
 var headerObj = {
-    ip:  req.ip,
-    Language: req.headers["accept-language"],
-    OS: req.headers["user-agent"]
+    ipaddress:  req.ip,
+    language: req.headers["accept-language"],
+    software: source
 }
     
 res.send(headerObj);
